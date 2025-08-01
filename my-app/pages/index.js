3bab527fbc339b1c54a2d7fc1525b89ec1a798ca
mohammadrashid <mohammadrashid6732@gmail.com>
2025-08-02 00:42:40 +0330

    add allbutton compoent for show all cars

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,9 +1,19 @@
-import Details from "./cars";
+import AllButton from "../components/module/AllButton";
+import Categories from "../components/module/Categories";
+import SearchPrice from "../components/module/SearchPrice";
+import CarsPage from "../components/templates/CarsPage";
+import carsData from "../data/carsData";
 
-export default function Home() {
+function Index() {
+  const cars = carsData.slice(0, 3);
   return (
     <div>
-      <Details />
+      <SearchPrice />
+      <Categories />
+      <AllButton />
+      <CarsPage data={cars} />
     </div>
   );
 }
+
+export default Index;
